6fe0b2777d0c80e5160debac3eecbb58e971298c
hydra <liuxinyuanxy@163.com>
2023-09-07 15:28:04 +0800

    final try

--- src/myrc.rs
+++ src/myrc.rs
@@ -11,8 +11,9 @@ pub struct MyRc<'a, T> {
 
 impl<'a, T> MyRc<'a, T> {
     pub fn new(data: T) -> Self {
+        let boxed = Box::new(RefCell::new(Inner { data, count: 1 }));
         MyRc {
-            ptr: &RefCell::new(Inner { data, count: 1 }),
+            ptr: Box::leak(boxed),
         }
     }
 
@@ -28,10 +29,11 @@ impl<'a, T> MyRc<'a, T> {
 
 impl<'a, T> Drop for MyRc<'a, T> {
     fn drop(&mut self) {
-        self.ptr.borrow_mut().count -= 1;
-        if self.ptr.borrow_mut().count == 0 {
-            println!("Dropping MyRc!");
-            std::mem::drop(self.ptr);
+        let mut inner = self.ptr.borrow_mut();
+        inner.count -= 1;
+        if inner.count == 0 {
+            std::mem::drop(inner);
+            println!("dropped")
         }
     }
 }
@@ -39,7 +41,7 @@ impl<'a, T> Drop for MyRc<'a, T> {
 impl<'a, T> std::ops::Deref for MyRc<'a, T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
-        &self.ptr.borrow().data
+        unsafe { &self.ptr.as_ptr().as_ref().unwrap().data }
     }
 }
 
@@ -53,16 +55,31 @@ mod tests {
     #[test]
     fn test_myrc() {
         let a: MyRc<i32> = MyRc::new(1);
+        assert_eq!(*a, 1);
         assert_eq!(a.strong_count(), 1);
-        let b: MyRc<i32> = a.clone();
+        let b = a.clone();
+        assert_eq!(*b, 1);
         assert_eq!(a.strong_count(), 2);
         assert_eq!(b.strong_count(), 2);
+
         {
-            let c: MyRc<i32> = a.clone();
+            let c = b.clone();
+            assert_eq!(*c, 1);
             assert_eq!(a.strong_count(), 3);
             assert_eq!(b.strong_count(), 3);
             assert_eq!(c.strong_count(), 3);
         }
+
         assert_eq!(a.strong_count(), 2);
+        assert_eq!(b.strong_count(), 2);
+
+        std::mem::drop(a);
+
+        assert_eq!(*b, 1);
+        assert_eq!(b.strong_count(), 1);
+
+        std::mem::drop(b);
+
+        println!("test_myrc finished")
     }
 }
